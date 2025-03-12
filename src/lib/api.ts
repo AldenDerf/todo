8d3b2111c7353370c6a87bdcda17c9f28fd9c051
Alden Derf <aldenderfc.fabro99@gmail.com>
2025-03-12 10:29:30 +0800

    added

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://todoapi.test/api',
+  baseURL: 'http://192.168.0.109:80/api',
 });
 
-export const getTodos = () => api.get<Todo[]>('/todos').then(res => res.data);
+export const getTodos = () => api.get<Todo[]>('/todos').then((res) => res.data);
 
 export const createTodo = (title: string) =>
-    api.post<Todo>('/todos', { title }).then(res => res.data);
+  api.post<Todo>('/todos', { title }).then((res) => res.data);
 
 export const updateTodo = (id: number, data: Partial<Todo>) =>
-    api.put<Todo>(`/todos/${id}`, data).then(res => res.data);
+  api.put<Todo>(`/todos/${id}`, data).then((res) => res.data);
 
-export const deleteTodo = (id: number) => 
-    api.delete(`/todos/${id}`).then(res => res.data);
+export const deleteTodo = (id: number) =>
+  api.delete(`/todos/${id}`).then((res) => res.data);
